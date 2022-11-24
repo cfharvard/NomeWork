0cbdcb50ce79ab6bc7d5ca7b50b38671a68f490a
cfharvard <112586108+cfharvard@users.noreply.github.com>
2022-11-24 13:36:57 -0500

    Added login_required

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, redirect, render_template, request, session
 from flask_session import Session
+from helpers import login_required
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/homepage")
+@login_required
+def homepage():
+    return render_template("homepage.html")
 
 @app.route("/login")
 def login():
