9aacf7eaf8917beb4641a9cf9d7f28a8f7109bee
cfharvard <112586108+cfharvard@users.noreply.github.com>
2022-11-24 12:53:25 -0500

    Adds login route

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
