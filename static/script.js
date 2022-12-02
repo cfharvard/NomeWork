94536f99076b5ae37ad2b50c978218320147d280
cfharvard <112586108+cfharvard@users.noreply.github.com>
2022-12-01 20:51:29 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -22,7 +22,7 @@ function timer() {
 
     // Format our time
     let hrs = Math.floor(seconds / 3600);
-    let mins = Math.floor(seconds - (hrs * 3600) / 60);
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60);
     let secs = seconds % 60;
     
     if (secs < 10) secs = '0' + secs;
