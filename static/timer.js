251ef4732253ec5bfedfb354323685629b788528
cfharvard <112586108+cfharvard@users.noreply.github.com>
2022-12-02 09:20:30 -0500

    Update timer.js

--- static/timer.js
+++ static/timer.js
@@ -64,7 +64,6 @@ function submit() {
             type:"POST",
             contentType: "application/json",
             data: JSON.stringify(seconds)});
-            console.log(1);
     }
     else
     {
