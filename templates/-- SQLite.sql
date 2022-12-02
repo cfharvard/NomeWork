83d68f65b3beedf61b43dcfe1bb976a40cedac82
hannawosenu <hannaawos@gmail.com>
2022-12-01 21:34:11 -0500

    sql

---
+++ templates/-- SQLite.sql
@@ -0,0 +1,20 @@
+-- SQLite
+-- CREATE TABLE users (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user TEXT NOT NULL,
+--     hash TEXT NOT NULL
+-- );
+
+-- CREATE TABLE classes (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name TEXT NOT NULL,
+--     user_id INTEGER NOT NULL,
+--     FOREIGN KEY (user_id) REFERENCES users (id)
+-- );
+
+-- CREATE TABLE times (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     class_id INTEGER NOT NULL,
+--     seconds INTEGER NOT NULL,
+--     FOREIGN KEY (class_id) REFERENCES classes(id)
+-- );
