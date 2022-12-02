a879ea603e2d7efe58699b1eda2153cca2e573e5
cfharvard <112586108+cfharvard@users.noreply.github.com>
2022-12-02 03:22:44 -0500

    t

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from helpers import login_required, generate_password_hash, check_password_hash
 import json
 import sqlite3
 
+
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 con = sqlite3.connect("nomework.db")
